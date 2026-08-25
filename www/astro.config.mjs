7863b1a8653f4954f1d72358c0af4ca8ce2071bf
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2026-08-25 18:33:37 -0500

    chore(www): default site URL to wholeearthcurriculum.org

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -6,7 +6,7 @@ import { copyAssets } from './src/lib/assets';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL || 'https://newworldorderedu.org',
+  site: process.env.SITE_URL || 'https://wholeearthcurriculum.org',
   output: 'static',
   trailingSlash: 'always',
   compressHTML: true,
